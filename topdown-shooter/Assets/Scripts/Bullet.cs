b35105eb62a889239cda763bc50eb5afc9d77542
hlindenau <hubert.lindenau@protonmail.com>
2020-11-06 17:57:28 +0100

    Add shooting
    Added shooting scripts and bullet hit particle effect.

---
+++ topdown-shooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    void OnCollisionEnter2D()
+    {
+        Destroy(this.gameObject);
+        GameObject explosion = Instantiate(hitEffect, this.GetComponent<Transform>().position, this.GetComponent<Transform>().rotation);
+        explosion.GetComponent<Transform>().transform.Rotate(170f, 0f, 0f);
+    }
+}
