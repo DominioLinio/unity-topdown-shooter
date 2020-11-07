7164d675eddbd3a5433909812fea997ed06d81f5
hlindenau <hubert.lindenau@protonmail.com>
2020-11-07 13:03:44 +0100

    Add treadmill object
    Added treadmill object which lets the player train to increase their movement speed value.

---
+++ topdown-shooter/Assets/Scripts/PlayerEvents.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEvents : MonoBehaviour
+{
+        public float speedModifier = 0.1f;
+        float speedLevel = 0f;
+        int currentClicks = 0;
+        bool onTreadmill = false;
+
+  
+    void Update()
+    {
+        if (onTreadmill && Input.GetKeyDown("space"))
+        {
+            TrainOnTreadmill();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Usable"){
+            Debug.Log("On treadmill");
+            onTreadmill = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Usable")
+        {
+            Debug.Log("Off treadmill");
+            onTreadmill = false;
+        }
+    }
+
+    void TrainOnTreadmill()
+    {
+        Debug.Log("Curent clicks: " + currentClicks);
+        currentClicks++;
+        if (currentClicks == Mathf.Pow(2, speedLevel))
+        {
+            speedLevel++;
+            currentClicks = 0;
+            Debug.Log(this.gameObject.GetComponent<PlayerMovement>().baseMoveSpeed + speedModifier * speedLevel);
+            this.gameObject.GetComponent<PlayerMovement>().baseMoveSpeed += speedModifier * speedLevel;
+        }
+    }
+
+}
--- topdown-shooter/Assets/Scripts/PlayerMovement.cs
+++ topdown-shooter/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float baseMoveSpeed = 1f;
     public float moveSpeed = 1f;
     public float speedModifier = 1f;
     float angle;
+    bool sprinting = false;
+
 
     public Rigidbody2D rb;
     public Camera cam;
@@ -16,6 +19,13 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift)) 
+        {
+            sprinting = true;
+            Debug.Log("sprinting");
+        }
+        else sprinting = false;
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
@@ -23,19 +33,19 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (sprinting)
+        {
+            moveSpeed = baseMoveSpeed + 2f;
+        }
+        else
+        {
+            moveSpeed = baseMoveSpeed;
+        }
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
 
         Vector2 lookDir = mousePos - rb.position;
 
         angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
-    }
-
-    void OnTriggerStay2D(Collider2D collider)
-    {
-        if (collider.gameObject.tag == "Usable")
-        {
-            Debug.Log("Player on usable object");
-        }
     }
 }
